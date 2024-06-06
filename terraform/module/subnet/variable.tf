92c5814a016c2ab83a09b284080975fffefa2ea3
Abhishek Yadav <144584287+Servzik@users.noreply.github.com>
2024-06-06 19:00:48 +0530

    Create variable.tf

---
+++ terraform/module/subnet/variable.tf
@@ -0,0 +1,9 @@
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "CIDR range for the subnet"
+  type        = string
+}
