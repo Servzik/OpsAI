6c2750d85396435dbf35a4a6f08438bd6f3f319d
Abhishek Yadav <144584287+Servzik@users.noreply.github.com>
2024-06-06 19:03:25 +0530

    Create variable.tf

---
+++ terraform/module/cluster/variable.tf
@@ -0,0 +1,9 @@
+variable "cluster_name" {
+  description = "Name of the cluster"
+  type        = string
+}
+
+variable "network_name" {
+  description = "Name of the network"
+  type        = string
+}
